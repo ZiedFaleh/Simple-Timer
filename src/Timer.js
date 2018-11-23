2994a8583fe32c36e95300ad8b747c11838ea222
ZiedFaleh <zied.faleh2@gmail.com>
2018-11-23 11:22:30 +0100

    first commit

---
+++ src/Timer.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+function Timing ()
+{
+
+
+ return  <div className="random-text">
+    
+    <div className="counter">
+    <p className="hours"> 
+        00
+    </p>
+    <p className="distance"> 
+        :
+    </p>
+    <p className="minutes"> 
+        30
+    </p>
+    <p className="distance"> 
+        :
+    </p>
+    <p className="seconds"> 
+        00
+    </p>
+    </div>
+    <div className="counter-txt">
+    <p className="hours-txt"> 
+        Hour
+    </p>
+    <p className="minutes-txt"> 
+        Minute
+    </p>
+    <p className="seconds-txt"> 
+        Second
+    </p>
+    </div>
+    </div>
+ 
+}
+export default Timing
